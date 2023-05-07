a958a9809de1f959831fb44b031a7184623883c9
ar9611 <airi.tsumura1352@gmail.com>
2023-05-07 19:39:09 +0900

    FMLARAVEL-26 4-11 viewとコントローラ

--- mynews/resources/views/admin/profile/edit.blade.php
+++ mynews/resources/views/admin/profile/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MyNews<</title>
+    <title>Myprofile</title>
   </head>
   <body>
     <h1>Myプロフィール編集画面</h1>
